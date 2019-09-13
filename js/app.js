fc34c0bc7b03d999f9e987a3acbbeba9cdaa2bb1
AiConan <3025945576@qq.com>
2019-09-13 10:42:21 +0800

    Commit

--- js/app.js
+++ js/app.js
@@ -81,7 +81,7 @@ var app = new Vue({
                     app.$refs.player.load();
                     mdui.updateSliders();
                     app.loading_done = true;
-                    mdui.JQ("[rel=\"shortcut icon\"]").attr("href",app.picurl)
+                    mdui.JQ("link[rel=\"shortcut icon\"]").attr("href",app.picurl);
                 }
             })
         },
