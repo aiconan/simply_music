a0ef7f6efeaf0ad623e8bb491acd74e07407adfe
aiconan <44314075+aiconan@users.noreply.github.com>
2019-10-26 21:04:12 +0800

    Update app.js
    Commit

--- js/app.js
+++ js/app.js
@@ -44,6 +44,7 @@ mdui.JQ(function(){
             main_scroll = new IScroll("main", scroll);
             mdui.JQ(".loading_page").remove();
             mdui.JQ("#app").show();
+            this.setvlume(localStorage.volume || 100);
             this.player(this.getQueryString("id") || 38592976);
         },
         methods: {
@@ -207,7 +208,9 @@ mdui.JQ(function(){
                 })
             },
             setvolume: function(value) {
+                this.volume = value;
                 this.$refs.player.volume = this.volume / 100;
+                localStorage.volume = this.volume / 100;
             },
             parseLyric: function(lrc){
                 var lyrics = lrc.split("\n");
